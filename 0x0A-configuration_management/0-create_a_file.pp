1d3001fd75c39a97acce77658392e62bce77923f
melkam5 <melkamargaw5@gmail.com>
2022-09-20 21:17:18 +0300

    task 0x0A

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
